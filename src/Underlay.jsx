2930c3fb1130e5bfffcf77571448fe8f7c72de5d
Austin Papritz <austin@papritz.dev>
2024-05-02 22:03:18 -0700

    added logo, update to color scheme

--- src/Underlay.jsx
+++ src/Underlay.jsx
@@ -12,7 +12,7 @@ const Grid = styled.div`
   font-family: 'ThunderBlack';
   font-size: 4em;
   line-height: 0.74em;
-  color: #f7f4af;
+  color: #fdb927;
 
   @media (max-width: 425px) {
     grid-template-columns: 1fr;
@@ -39,22 +39,22 @@ const Right = styled.div`
   display: flex;
   width: 100%;
   height: 100%;
-  background: #f7f4af;
-  color: #252525;
+  background: #fdb927;
+  color: #26282a;
 `
 
 const Sub = styled.div`
   align-self: end;
   width: 200px;
   height: 2px;
-  background: #f7f4af;
+  background: #006bb6;
 `
 
 const Jumbo = styled.div`
   align-self: center;
   padding: 100px;
-  font-size: 3.5em;
-  color: #252525;
+  font-size: 3em;
+  color: #006bb6;
 `
 
 const Label = styled.div`
@@ -64,7 +64,7 @@ const Label = styled.div`
   font-family: 'ThunderThin';
   font-size: 0.2em;
   line-height: 1em;
-  color: #252525;
+  color: #26282a;
   padding: 100px;
 
   @media (max-width: 768px) {
@@ -81,21 +81,18 @@ export default function Underlay() {
     <Grid>
       <Left>
         <div>
-          THE SISTRUM
+          GOLDEN
           <br />
-          IS DESIGNED
+          STATE
           <br />
-          TO REPRESENT
-          <br />
-          CONTINUAL
-          <br />
-          AGITATION.
+          <img src="./Golden-State-Warriors-Logo-PNG-Picture.png" alt="Golden State Warriors" height="200px" />
         </div>
         <Sub />
       </Left>
       <Right>
-        <Jumbo>M1</Jumbo>
+        <Jumbo>WARRIORS</Jumbo>
       </Right>
+      <Label>EST. 1946</Label>
     </Grid>
   )
 }
